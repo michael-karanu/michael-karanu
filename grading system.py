875fcf29aa79ba474a1b5f7bd445562a1b3f213d
michael-karanu <105698243+michael-karanu@users.noreply.github.com>
2022-05-20 11:38:40 +0300

    Add files via upload

---
+++ grading system.py
@@ -0,0 +1,11 @@
+grade = int(input("enter student grade"))
+if grade >= 80 and grade <= 100:
+    print("student has an A")
+if grade >= 70 and grade < 80:
+    print("student has a B")
+if grade >= 60 and grade < 70:
+    print("student has a C")
+if grade >= 50 and grade < 60:
+    print("student has a D")
+if grade >= 0 and grade < 50:
+    print("student has an E")
---
+++ lists.py
@@ -0,0 +1,19 @@
+words = ["apples","love","people","!"]
+print(words[0])
+print(words[1])
+print(words)
+dat = ['a',1,'too',6,7,"hey"]
+print(dat)
+m = [
+    [5,6,7],
+    [8,9,2]
+]
+print(m[1][2])
+print(m)
+strg = ["hello","class","123","st","abc","hey",'b','a']
+print(strg)
+print(strg[5])
+print(strg[6])
+print(strg[7-3])
+strg[0] = strg[7-2]
+print(strg[7-2])  
